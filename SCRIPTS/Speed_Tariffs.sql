11c118b01355ed056eec5eea3ebb887c57169e9d
net-maestro <data.happy.link@gmail.com>
2026-04-09 20:12:28 +0300

    Update Speed_Tariffs.sql

--- SCRIPTS/Speed_Tariffs.sql
+++ SCRIPTS/Speed_Tariffs.sql
@@ -30,3 +30,24 @@ LEFT JOIN addr_groups gr on gr.id = ha.group_id
 LEFT JOIN client_prices pr on pr.agreement = s.id and pr.time_stop is null
 LEFT JOIN bill_prices pn on pn.id = pr.price
 WHERE s.agreement in (51004, 51005, 51010, 51012, 51020, 51029, 51033, 51034, 51035, 51036, 51037, 51038, 51043, 51044, 51046, 51047, 51048, 51054, 51057, 51061, 51067, 51069, 51071, 51073, 51076, 51078, 51081, 51090, 51093, 51101, 51102, 51105, 51111, 51118, 51125, 51129, 51130, 51131, 51144, 51145, 51147, 52008, 53038, 53079, 53146, 53305, 53400) AND pn.name='Білий IP'
+
+
+
+sql = """
+SELECT
+    b.switch,
+                b.mac,
+                b.port,
+                b.ip
+FROM eq_bindings b
+JOIN client_prices act ON act.id = b.activation
+JOIN clients cl ON cl.id = act.agreement
+WHERE b.switch=265 AND cl.agreement IN (
+    51004, 51005, 51010, 51012, 51020, 51029, 51033, 51034, 51035, 51036,
+    51037, 51038, 51043, 51044, 51046, 51047, 51048, 51054, 51057, 51061,
+    51067, 51069, 51071, 51073, 51076, 51078, 51081, 51090, 51093, 51101,
+    51102, 51105, 51111, 51118, 51125, 51129, 51130, 51131, 51144, 51145,
+    51147, 52008, 53038, 53079, 53146, 53305, 53400
+);
+"""
+
